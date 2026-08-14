d79ac630c48883cedd7f8187b6b1ba591dc04662
Guillermo Ruiz de Azúa Sánchez Izquierdo <202300876@alu.comillas.edu>
2026-08-14 20:41:38 +0200

    fix: fixed error trying to unpack a None

--- cvrptw/heuristics/construction.py
+++ cvrptw/heuristics/construction.py
@@ -42,7 +42,7 @@ def build_initial_solution(instance: Instance) -> Solution:
     routes = []
 
     while nodes:
-        # start the route with the farder node from the depot
+        # start the route with the further node from the depot
         further = nodes[0]
         for i in nodes:
             if instance.distance(0, i) > instance.distance(0, further):
@@ -51,7 +51,8 @@ def build_initial_solution(instance: Instance) -> Solution:
         nodes.remove(further)
 
         # keep inserting nodes with the cheapest distance cost
-        while node_pick != None:
+        inserting = True
+        while inserting:
             node_pick = None
             for i in nodes:
                 result = best_node_insertion(instance, route, i)
@@ -59,9 +60,12 @@ def build_initial_solution(instance: Instance) -> Solution:
                     pos, extra = result
                     if node_pick is None or extra < node_pick[2]:
                         node_pick = (i, pos, extra)
-            i, pos, extra = node_pick
-            route.insert(pos, i)
-            nodes.remove(i)
+            if node_pick is None:
+                inserting = False
+            else:
+                i, pos, extra = node_pick
+                route.insert(pos, i)
+                nodes.remove(i)
         routes.append(route)
 
     return Solution(instance, routes)
