0a1a0a268a2f72f97a1e9cf2d92e7db90aeda30b
Guillermo Ruiz de Azúa Sánchez Izquierdo <202300876@alu.comillas.edu>
2026-08-14 12:43:08 +0200

    chore: removed unused files

--- cvrptw/__init__.py
+++
@@ -1,5 +0,0 @@
-"""CVRPTW: Capacitated Vehicle Routing Problem with Time Windows.
-
-Exact formulation (Gurobi), a custom ALNS metaheuristic, and wrappers around
-industry solvers, benchmarked on the Solomon instances.
-"""
